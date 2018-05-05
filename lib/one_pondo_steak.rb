f24670bb004a6815b9f8e61a2f6acb8c061c9223
mizutamazukki <mickydisney1212@gmail.com>
2018-05-05 20:39:47 +0900

    first commitush

--- lib/one_pondo_steak.rb
+++ lib/one_pondo_steak.rb
@@ -2,6 +2,11 @@ require 'one_pondo_steak/version'
 
 #require 'one_pondo_steak/product'
 module OnePondoSteak
+  class Hoge
+    def self.home
+      p 'asd'
+    end
+  end
   require 'one_pondo_steak/common'
   require 'one_pondo_steak/datetime_param'
   require 'one_pondo_steak/string_param'
