0aec34f5a9b8bb98a5d82bac20a1e738fc3864d9
mizutamazukki <mickydisney1212@gmail.com>
2018-05-05 20:57:13 +0900

    first commitush

--- lib/one_pondo_steak.rb
+++ lib/one_pondo_steak.rb
@@ -1,16 +1,22 @@
 require 'one_pondo_steak/version'
-
+require 'one_pondo_steak/common'
+require 'one_pondo_steak/datetime_param'
+require 'one_pondo_steak/string_param'
+require 'one_pondo_steak/numeric_param'
+require 'one_pondo_steak/regex'
+require 'one_pondo_steak/user'
 #require 'one_pondo_steak/product'
-module OnePondoSteak
+class OnePondoSteak
   class Hoge
     def self.home
       p 'asd'
     end
   end
-  require './one_pondo_steak/common'
-  require 'one_pondo_steak/datetime_param'
-  require 'one_pondo_steak/string_param'
-  require 'one_pondo_steak/numeric_param'
-  require 'one_pondo_steak/regex'
-  require 'one_pondo_steak/user'
+  include 'one_pondo_steak/common'
+  include 'one_pondo_steak/datetime_param'
+  include 'one_pondo_steak/string_param'
+  include 'one_pondo_steak/numeric_param'
+  include 'one_pondo_steak/regex'
+  include 'one_pondo_steak/user'
+
 end
