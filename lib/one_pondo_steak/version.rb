23187a38a53afb7cad873abe901362278966fa0f
mizutamazukki <mickydisney1212@gmail.com>
2018-05-06 14:29:45 +0900

    refactoring gem

--- lib/one_pondo_steak/version.rb
+++
@@ -1,3 +0,0 @@
-module OnePondoSteak
-  VERSION = "0.1.0"
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,1 @@
-require File.expand_path('../../lib/one_pondo_steak', __FILE__)
+require File.expand_path('../../lib/one_pound_steak', __FILE__)
