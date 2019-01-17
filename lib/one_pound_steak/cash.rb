629ea246287d120dd05d50591ef7b4fcc3647d2b
spica831 <h11404m@gmail.com>
2019-01-17 15:32:58 +0900

    fix

--- lib/one_pound_steak/cash.rb
+++ lib/one_pound_steak/cash.rb
@@ -4,56 +4,48 @@ module OnePoundSteak
   class Cash < Common
 
     def stripe_card_id(param)
-      Common.not_nil_check(__method__.to_s, param)
+      Common.not_nil_check('stripe_card_id', param)
     end
 
     def coupon_code(param)
-      Common.not_nil_check(__method__.to_s, param)
+      Common.not_nil_check('coupon_code', param)
     end
 
     def coupon_code_or_stripe_card_id(coupon_code, stripe_card_id)
-      is_valid_coupon, _  = Common.not_nil_check(__method__.to_s, coupon_code)
-      is_valid_card_id, _ = Common.not_nil_check(__method__.to_s, stripe_card_id)
+      is_valid_coupon, _  = Common.not_nil_check('coupon_code', coupon_code)
+      is_valid_card_id, _ = Common.not_nil_check('stripe_card_id', stripe_card_id)
 
       return false, 'empty params coupon_code or stripe_card_token' unless is_valid_coupon || is_valid_card_id
 
       [true, 'ok ' + __method__.to_s]
     end
 
-    def stripe_card_token_or_coupon_code?
-      is_token_nil = @stripe_card_token.nil?
-      is_coupon_nil = @coupon_code.nil?
-      @message.push('empty params coupon_code or stripe_card_token') if is_coupon_nil && is_token_nil
-      @message.push('delete params coupon_code or stripe_card_token') unless is_token_nil || is_coupon_nil
-      !(is_coupon_nil && is_token_nil) && (is_token_nil || is_coupon_nil)
-    end
-
     def order_num(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_NUTURAL_NUMBER)
+      Common.regex_check('order_num', param, Regex::VALID_NUTURAL_NUMBER)
     end
 
     def product_id(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_NUTURAL_NUMBER)
+      Common.regex_check('product_id', param, Regex::VALID_NUTURAL_NUMBER)
     end
 
     def acceptable_date(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_DATETIME)
+      Common.regex_check('acceptable_date', param, Regex::VALID_DATETIME)
     end
 
     def total_price(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_NUTURAL_NUMBER)
+      Common.regex_check('total_price', param, Regex::VALID_NUTURAL_NUMBER)
     end
 
     def stripe_varsion(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_DATE)
+      Common.regex_check('stripe_varsion', param, Regex::VALID_DATE)
     end
 
     def cash_id(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_NUTURAL_NUMBER)
+      Common.regex_check('cash_id', param, Regex::VALID_NUTURAL_NUMBER)
     end
 
     def page(param)
-      Common.regex_check(__method__.to_s, param, Regex::VALID_NUTURAL_NUMBER)
+      Common.regex_check('page', param, Regex::VALID_NUTURAL_NUMBER)
     end
 
   end
